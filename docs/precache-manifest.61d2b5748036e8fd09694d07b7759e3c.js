42917285edc9acbb5654d3fc1e865d0e36219150
Luis Borrego <luevech@gmail.com>
2020-07-30 14:22:54 +0200

    docs add

---
+++ docs/precache-manifest.61d2b5748036e8fd09694d07b7759e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1001578cce9c17fbabf5cfe32c27801f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ad095bfe9f6c9672358",
+    "url": "/static/css/main.41ab7272.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ad095bfe9f6c9672358",
+    "url": "/static/js/main.8ba3eeff.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
